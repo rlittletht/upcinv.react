05b044e7d0a7b3e8cecdb1d4a2693dff4dc933c0
Rob Little <rlittletht@users.noreply.github.com>
2019-07-28 19:06:40 -0700

    Make this a react web app (reference: https://docs.microsoft.com/en-us/visualstudio/javascript/tutorial-nodejs-with-react-and-jsx?view=vs-2019)

---
+++ UpcInv.React/app.tsx
@@ -0,0 +1,25 @@
+﻿declare var require: any
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+export class Hello extends React.Component {
+    render() {
+        return (
+            <h1>Welcome to <Inner/></h1>
+        );
+    }
+}
+
+export class Inner extends React.Component
+{
+    render()
+    {
+        return (
+            <span style={{"font-weight": "bold"}}>This is an inline react test string</span>
+        );
+    }
+}
+
+
+ReactDOM.render(<Hello />, document.getElementById('root'));
